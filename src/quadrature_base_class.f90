3b21f724983e13842b118e3d03e4d95f6fbc4fcd
Ian <my.business@not.yours>
2020-05-13 14:08:23 +0100

    Added quadrature class in prep for parallelisation

---
+++ src/quadrature_base_class.f90
@@ -0,0 +1,28 @@
+Module quadrature_base_module
+
+  Use, Intrinsic :: iso_fortran_env, Only :  wp => real64
+
+  Implicit None
+
+  Type, Public, Abstract :: quadrature_base_class
+     Private
+   Contains
+     Procedure( integrate ), Deferred, NoPass, Public :: integrate
+  End Type quadrature_base_class
+
+  Abstract Interface
+     
+     Pure Function integrate( l, n_grid, grid ) Result( r )
+       Use lattice_module, Only : lattice
+       Import :: wp
+       Real( wp ) :: r
+       Type( lattice )                 , Intent( In ) :: l
+       Integer   , Dimension( :       ), Intent( In ) :: n_grid
+       Real( wp ), Dimension( :, :, : ), Intent( In ) :: grid
+     End Function integrate
+     
+  End Interface
+
+  Private
+  
+End Module quadrature_base_module
---
+++ src/quadrature_trapezium_serial.f90
@@ -0,0 +1,58 @@
+Module quadrature_trapezium_serial_module
+
+  Use, Intrinsic :: iso_fortran_env, Only :  wp => real64
+
+  Use quadrature_base_module, Only : quadrature_base_class
+
+  Implicit None
+
+  Type, Public, Extends( quadrature_base_class ) :: quadrature_trapezium_serial
+     Private
+   Contains
+     Procedure, NoPass, Public :: integrate => trapezium_serial
+  End type quadrature_trapezium_serial
+
+Contains
+
+  Pure Function trapezium_serial( l, n_grid, grid ) Result( r )
+
+    Use, Intrinsic :: iso_fortran_env, Only :  wp => real64
+
+    Use lattice_module, Only : lattice
+
+    Real( wp ) :: r
+
+    Type( lattice )                 , Intent( In ) :: l
+    Integer   , Dimension( :       ), Intent( In ) :: n_grid
+    Real( wp ), Dimension( :, :, : ), Intent( In ) :: grid
+
+    Real( wp ) :: dV
+    Real( wp ) :: c, y, t
+
+    Integer :: i1, i2, i3
+
+
+    dV =  l%get_volume() / Product( n_grid )
+!!$    r  = Sum( grid ) * dV
+    ! Integrate using Kahan summation for accuracy
+    r = 0.0_wp
+    !$ omp parallel default( none ) shared( grid, r ) private( c, y, t, i1, i2, i3 )
+    c = 0.0_wp
+    !$omp do collapse( 3 ) reduction( +:r )
+    Do i3 = Lbound( grid, Dim = 3 ), Ubound( grid, Dim = 3 )
+       Do i2 = Lbound( grid, Dim = 2 ), Ubound( grid, Dim = 2 )
+          Do i1 = Lbound( grid, Dim = 1 ), Ubound( grid, Dim = 1 )
+             y = grid( i1, i2, i3 ) - c
+             t = r + y
+             c = ( t - r ) - y
+             r = t
+          End Do
+       End Do
+    End Do
+    !$omp end do
+    !$omp end parallel
+    r = r * dV    
+    
+  End Function trapezium_serial
+  
+End Module quadrature_trapezium_serial_module
