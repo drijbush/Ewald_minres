81a0bf4d9f8563fc9512c84353e7809014fe557a
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2020-07-16 15:07:50 +0100

    Cleanup

--- test/exp.f90
+++ test/exp.f90
@@ -5,25 +5,23 @@ program test_exp
 
   Real( wp ), Dimension(3) :: r_0 = 0.0_wp, r
   Real( wp ) :: g_r
-  Real( wp ), Dimension(3) :: g_r0, g_2dr, g_dr, f_rdr, f_rdr0, f_rdr00, f_drdr
+  Real( wp ), Dimension(3) :: g_r0, g_dr, f_rdr, f_rdr0, f_rdr00, f_drdr
   Real( wp ), Dimension(3,3) :: g_drdr
   Real( wp ), Dimension(3,3), Parameter :: dr = Reshape([&
        0.01_wp, 0.01_wp, 0.03_wp, &
        0.02_wp, 0.04_wp, 0.07_wp, &
        0.03_wp, -0.05_wp, 0.02_wp], [3,3])
   Real( wp ), Parameter :: alpha = 0.4_wp
-  Integer,    Parameter :: nSamp = 10
+  Integer,    Parameter :: nSamp = 16
   Integer :: i, j, k
 
-  r = r_0
+  r = r_0 - sum(nSamp*dr, dim=2)
 
   g_r0 = g(r, alpha)
   g_r = g_r0(1)
 
   do i = 1, 3
-    print*, dr(:, i)
     g_dr(i) = g(dr(:, i), alpha)
-    g_2dr(i) = g( Sqrt( 2.0_wp ) * dr(:, i), alpha)
     f_rdr0(i) = f(r, dr(:, i), alpha)
     f_drdr(i) = f(dr(:, i), dr(:, i), alpha)
   end do
@@ -32,23 +30,18 @@ program test_exp
     do i = 1, 3
       g_drdr(i, j) = f(dr(:,i), dr(:,j), alpha)
     end do
-    print*, g_2dr(j)
-    print*, g_drdr(:,j)
   end do
-
-
 
   f_rdr00 = f_rdr0
   f_rdr = f_rdr0
 
-  i=0;j=0;k=0
-
-  print*, 'i   j   k     rx     ry     rz           apprx                exact                 diff'
-  do k = 0, 4
-     do j = 0, 4
-       do i = 0, 4
-        r = r_0 + i*dr(:, 1) + j*dr(:, 2) + k*dr(:, 3)
-        print('(3(i3.1, 1X),1X,3(f6.3,1X),1X,3(g21.15, 1x) )'), i, j, k, r, g_r, g(r, alpha), g_r - g(r, alpha)
+  do k = -nSamp, nSamp
+     do j = -nSamp, nSamp
+       do i = -nSamp, nSamp
+         ! r used only for printing
+         r = r_0 + i*dr(:, 1) + j*dr(:, 2) + k*dr(:, 3)
+         print('(3(i3.1, 1X),1X,3(f6.3,1X),1X,3(g21.15, 1x) )'), &
+              i, j, k, r, g_r, g(r, alpha), g_r - g(r, alpha)
         ! print*, "INFO ",i, j, k, g_r - g(r, alpha)
         ! print*, "F_RDR", f_rdr
         ! print*, "EXACT", [(f(r, dr(:, i), alpha), i=1,3)]
@@ -58,18 +51,15 @@ program test_exp
         f_rdr = f_rdr * g_drdr(:, 1)
       end do
 
-      g_r = g_r0(2)
-      g_r = g_r * f_rdr0(2) * g_dr(2)
+      g_r = g_r0(2) * f_rdr0(2) * g_dr(2)
       g_r0(2) = g_r
 
       f_rdr = f_rdr0 * g_drdr(:, 2)
       f_rdr0 = f_rdr
     end do
 
-    g_r = g_r0(1)
-    g_r = g_r * f_rdr00(3) * g_dr(3)
-    g_r0(1) = g_r
-    g_r0(2) = g_r
+    g_r = g_r0(1) * f_rdr00(3) * g_dr(3)
+    g_r0 = g_r
 
     f_rdr = f_rdr00 * g_drdr(:, 3)
     f_rdr0 = f_rdr
