7f749866c06235e13ecb3b147eb6353dde96d581
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2020-07-16 14:39:15 +0100

    Working for general vectors

--- test/exp.f90
+++ test/exp.f90
@@ -8,9 +8,9 @@ program test_exp
   Real( wp ), Dimension(3) :: g_r0, g_2dr, g_dr, f_rdr, f_rdr0, f_rdr00, f_drdr
   Real( wp ), Dimension(3,3) :: g_drdr
   Real( wp ), Dimension(3,3), Parameter :: dr = Reshape([&
-       0.01_wp, 0.01_wp, - 0.01_wp, &
-       0.02_wp, 0.02_wp, 0.05_wp, &
-       0.10_wp, -0.03_wp, 0.03_wp], [3,3])
+       0.01_wp, 0.01_wp, 0.03_wp, &
+       0.02_wp, 0.04_wp, 0.07_wp, &
+       0.03_wp, -0.05_wp, 0.02_wp], [3,3])
   Real( wp ), Parameter :: alpha = 0.4_wp
   Integer,    Parameter :: nSamp = 10
   Integer :: i, j, k
@@ -48,40 +48,32 @@ program test_exp
      do j = 0, 4
        do i = 0, 4
         r = r_0 + i*dr(:, 1) + j*dr(:, 2) + k*dr(:, 3)
-        ! print('(3(i3.1, 1X),1X,3(f6.3,1X),1X,3(g21.15, 1x) )'), i, j, k, r, g_r, g(r, alpha), g_r - g(r, alpha)
-        print*, "INFO ",i, j, k, g_r - g(r, alpha)
-        print*, "F_RDR", f_rdr
-        print*, "EXACT", [(f(r, dr(:, i), alpha), i=1,3)]
-        print*, "DIFF ", f_rdr - [(f(r, dr(:, i), alpha), i=1,3)]
+        print('(3(i3.1, 1X),1X,3(f6.3,1X),1X,3(g21.15, 1x) )'), i, j, k, r, g_r, g(r, alpha), g_r - g(r, alpha)
+        ! print*, "INFO ",i, j, k, g_r - g(r, alpha)
+        ! print*, "F_RDR", f_rdr
+        ! print*, "EXACT", [(f(r, dr(:, i), alpha), i=1,3)]
+        ! print*, "DIFF ", f_rdr - [(f(r, dr(:, i), alpha), i=1,3)]
 
-        g_r = g_r * f_rdr(1) * g_drdr(1,1)
+        g_r = g_r * f_rdr(1) * g_dr(1)
         f_rdr = f_rdr * g_drdr(:, 1)
-        ! f_rdr(2) = f_rdr(2) * g_2dr(1)  ! Temp
-        ! f_rdr(3) = f_rdr(3) * g_2dr(1)  ! Temp
       end do
+
       g_r = g_r0(2)
-      g_r = g_r * f_rdr0(2) * g_drdr(2,2)
+      g_r = g_r * f_rdr0(2) * g_dr(2)
       g_r0(2) = g_r
-      ! f_rdr(2) = f_rdr(2) * g_2dr(2)
+
       f_rdr = f_rdr0 * g_drdr(:, 2)
       f_rdr0 = f_rdr
-      ! f_rdr0(1) = f_rdr(1)
-      ! f_rdr(3) = f_rdr0(3) ! Temp
     end do
+
     g_r = g_r0(1)
-    g_r = g_r * f_rdr0(3) * g_drdr(3,3)
+    g_r = g_r * f_rdr00(3) * g_dr(3)
     g_r0(1) = g_r
     g_r0(2) = g_r
+
     f_rdr = f_rdr00 * g_drdr(:, 3)
     f_rdr0 = f_rdr
     f_rdr00 = f_rdr
-    ! f_rdr(3) = f_rdr0(3) ! Temp
-    ! f_rdr(3) = f_rdr(3) * g_2dr(3)
-    ! f_rdr0(3) = f_rdr(3) ! Temp
-    ! f_rdr(2) = f_rdr0(2) * g_2dr(3)
-    ! f_rdr0(2) = f_rdr(2)
-    ! f_rdr(1) = f_rdr00(1) * g_2dr(3)
-    ! f_rdr0(1) = f_rdr(1)
   end do
 
 contains
