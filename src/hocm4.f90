abe2be41b820be05ae159e4be30be8a4003ec985
Ian <my.business@not.yours>
2021-02-06 23:51:48 +0000

    Early hocm work

---
+++ src/hocm4.f90
@@ -0,0 +1,8 @@
+Module FD_Laplacian_3d_hocm4_module
+
+  Use numbers_module        , Only : wp
+  Use FD_Laplacian_3d_module, Only : FD_Laplacian_3d
+
+  Implicit None
+  
+End Module FD_Laplacian_3d_hocm4_module
